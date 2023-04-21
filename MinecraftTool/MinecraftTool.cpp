b6528ef5cc088a1c394e223f36eb3a8af1efbf25
Twannes <Twannes02@gmail.com>
2023-04-22 00:53:10 +0200

    Finished first week assignment
    Starting the program by calling it on the cmd with given parameters
    Optimizations

--- MinecraftTool/MinecraftTool.cpp
+++ MinecraftTool/MinecraftTool.cpp
@@ -1,216 +1,304 @@
+#include <codecvt>
+
 #include "rapidjson.h"
 #include "document.h"
 #include "istreamwrapper.h"
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <vector>
 
-#include "vld.h"
+//#include "vld.h"
 
-void FirstPartLes1();
-void MinecraftJSON();
+struct Vector3
+{
+	float x{}, y{}, z{};
+};
+
+struct Block
+{
+	Vector3 position{};
+
+	std::wstring layer{};
+
+	bool isOpaque{};
+};
+
+
+std::vector<Block> g_Blocks;
+
+void Help();
+void WriteAwayBlock(FILE* pOutputFile, const Block& block, const int offset);
 
 int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 {
 
-	FILE* pOutputFile = nullptr;
+	std::wstring inputFileName{};
+	std::wstring outputFileName{};
 
-	_wfopen_s(&pOutputFile, L"sceneminecraft_zelf.obj", L"w+,ccs=UTF-8");
+	const std::wstring dotJson{L".json"};
+	const std::wstring dotObj{L".obj"};
 
-	const wchar_t* text = L" # is the symbol for partial derivative.\n";
-	fwrite(text, wcslen(text) * sizeof(wchar_t), 1, pOutputFile);
-
-	fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", 0.0f, 0.0f, 1.0f);
-	fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", 0.0f, 0.0f, -1.0f);
-	fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", 0.0f, 1.0f, 0.0f);
-	fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", 0.0f, -1.0f, 0.0f);
-	fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", 1.0f, 0.0f, 0.0f);
-	fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n", -1.0f, 0.0f, 0.0f);
-
-	if(std::ifstream inputStream{"minecraftscene.json"})
+	//check for right arguments
+	if(argc != 3 && argc != 5)
 	{
+		Help();
+		return -1;
+	}
+
+	for(int i = 1; i < argc; i += 2)
+	{
+
+		std::wstring argument = argv[i];
+
+		//check if argument is not size 2
+		if(argument.size() != 2)
+		{
+			Help();
+			return -1;
+		}
+
+		//switch over the -i / -o
+		switch (argument[1])
+		{
+			case 'i':
+			{
+				inputFileName = argv[i + 1];
+
+				//check if file extension is right
+				if (inputFileName.size() <= 5 || inputFileName.substr(inputFileName.size() - 5) != dotJson)
+				{
+					wprintf_s(L"inputFile needs to have at least one character and must end with .json\n");
+					Help();
+					return -1;
+				}
+			}
+			break;
+			case 'o':
+			{
+				outputFileName = argv[i + 1];
+
+				//check if file extension is right
+				if (outputFileName.size() <= 4 || outputFileName.substr(outputFileName.size() - 4) != dotObj)
+				{
+					wprintf_s(L"outputFile needs to have at least one character and must end with .obj\n");
+					Help();
+					return -1;
+				}
+			}
+			break;
+			default:
+			{
+				//if wrong send help
+				Help();
+				return -1;
+			}
+		}
+
+
+	}
+
+	//if no output defined, define output
+	if(outputFileName.empty())
+	{
+		outputFileName = inputFileName;
+
+		//replace .json with .obj
+		outputFileName.replace(outputFileName.size() - 5, 5, dotObj);
+	}
+
+	//open input file
+	if (std::ifstream inputStream{ inputFileName })
+	{
+
 		using namespace rapidjson;
+
+		//making variables for json reader
 		IStreamWrapper isw{ inputStream };
 		Document sceneDoc;
 		sceneDoc.ParseStream(isw);
 
-		if(sceneDoc.IsArray())
+		if (sceneDoc.IsArray())
 		{
-			int cubeIdx{};
+			//converter for the getstring
+			//https://stackoverflow.com/a/73412919
+			std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> UTF_Converter;
 
-			for(auto it = sceneDoc.Begin(); it != sceneDoc.End(); ++it)
+			for (auto it = sceneDoc.Begin(); it != sceneDoc.End(); ++it)
 			{
-				const Value& cube = *it;
+				//make new block
+				Block newBlock{};
 
-				if(cube.HasMember("layer") && cube.HasMember("opaque") && cube.HasMember("positions"))
+				const Value& blockInfo{ *it };
+
+				//check if variables are correct
+				if (blockInfo.HasMember("layer") && blockInfo.HasMember("opaque") && blockInfo.HasMember("positions"))
 				{
-					const Value& pos = cube["positions"]; 
 
-					int posIdx{};
+					//fill in opaque and layer
+					const Value& opaque{ blockInfo["opaque"] };
 
-					for (auto posIt = pos.Begin(); posIt != pos.End(); ++posIt)
+					if (opaque.IsBool()) newBlock.isOpaque = opaque.GetBool();
+
+					const Value& layer{ blockInfo["layer"] };
+
+					if (layer.IsString()) newBlock.layer = UTF_Converter.from_bytes(layer.GetString());
+
+					const Value& positions{ blockInfo["positions"] };
+
+					if (positions.IsArray())
 					{
-						const Value& currentPos = pos[posIdx];
 
-						if (pos.IsArray() && currentPos.Size() == 3)
+						for (auto posIt = positions.Begin(); posIt != positions.End(); ++posIt)
 						{
+							const Value& currentPos{ *posIt };
 
-							const Value& xVal = currentPos[1];
-							const Value& yVal = currentPos[2];
-							const Value& zVal = currentPos[0];
+							if (currentPos.IsArray() && currentPos.Size() == 3)
+							{
+								const Value& x = currentPos[1];
+								const Value& y = currentPos[2];
+								const Value& z = currentPos[0];
 
-							if ((xVal.IsInt() && yVal.IsInt() && zVal.IsInt()) == false) return 0;
+								if (x.IsInt() && y.IsInt() && z.IsInt())
+								{
+									//fill in new pos
+									Vector3 newPos{};
 
-							const float x = xVal.GetFloat();
-							const float y = yVal.GetFloat();
-							const float z = zVal.GetFloat();
+									newPos.x = static_cast<float>(x.GetInt());
+									newPos.y = static_cast<float>(y.GetInt());
+									newPos.z = static_cast<float>(z.GetInt());
 
-							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", x, y, z);
-							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", x, y, z + 1.0f);
-							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", x, y + 1.0, z);
-							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", x, y + 1.0, z + 1.0f);
-							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", x + 1.0, y, z);
-							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", x + 1.0, y, z + 1.0f);
-							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", x + 1.0, y + 1.0, z);
-							fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", x + 1.0, y + 1.0, z + 1.0f);
+									//when everything is correct emplace new block
+									g_Blocks.emplace_back(newBlock);
 
-							const int offset = cubeIdx * 8;
-
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 2, 7 + offset, 2, 5 + offset, 2);
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 2, 3 + offset, 2, 7 + offset, 2);
-
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1+ offset, 6, 4 + offset, 6, 3 + offset, 6);
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1+ offset, 6, 2 + offset, 6, 4 + offset, 6);
-
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + offset, 3, 8 + offset, 3, 7 + offset, 3);
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + offset, 3, 4 + offset, 3, 8 + offset, 3);
-
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + offset, 5, 7 + offset, 5, 8 + offset, 5);
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + offset, 5, 8 + offset, 5, 6 + offset, 5);
-
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 4, 5 + offset, 4, 6 + offset, 4);
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 4, 6 + offset, 4, 2 + offset, 4);
-
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + offset, 1, 6 + offset, 1, 8 + offset, 1);
-							fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + offset, 1, 8 + offset, 1, 4 + offset, 1);
-
-							++cubeIdx;
-							++posIdx;
+								}
+							}
 						}
 					}
-
 				}
-
 			}
+		}
+	}
+	else
+	{
+		//if file couldnt open return
+		wprintf_s(L"Couldn't open inputfile\n");
+		wprintf_s(L"Make sure you either have the file or named it wrong\n");
+		Help();
+		return -1;
+	}
+
+	//create outputfile
+
+	FILE* pOutputFile{};
+
+	_wfopen_s(&pOutputFile, outputFileName.c_str(), L"w+,ccs=UTF-8");
+
+	if(pOutputFile)
+	{
+
+		//create offset for the indices
+		constexpr int indiceOffset{ 8 };
+
+		//write away comment
+		const wchar_t* text = L" # is the symbol for partial derivative.\n";
+		fwrite(text, wcslen(text) * sizeof(wchar_t), 1, pOutputFile);
+
+		//write away the material library
+		fwprintf_s(pOutputFile, L"mtllib scene.mtl\n\n");
+
+		//write away all the different normals
+		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n",   0.0f, 0.0f, 1.0f);
+		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n",   0.0f, 0.0f, -1.0f);
+		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n",   0.0f, 1.0f, 0.0f);
+		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n",   0.0f, -1.0f, 0.0f);
+		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n",   1.0f, 0.0f, 0.0f);
+		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n\n", -1.0f, 0.0f, 0.0f);
+
+		int currIdx{};
+
+		for(const auto& block : g_Blocks)
+		{
+			const Block& blockInfo{ block };
+
+			//print the layer
+			fwprintf_s(pOutputFile, L"usemtl %ls\n", block.layer.c_str());
+
+			//print the block
+			WriteAwayBlock(pOutputFile, blockInfo, currIdx);
+
+			//print enter after done
+			fwprintf_s(pOutputFile, L"\n");
+
+			//offset the index
+			currIdx += indiceOffset;
 		}
 
 		fclose(pOutputFile);
+
+		wprintf_s(L"Operation succeeded\n");
 	}
-
-	//todo add positions to vertices
-
-	//eerst alle vertices wegschrijven dan correcte indices wegschrijven
-
-	//lijn beginnen met "g" Cube0 --> dan worden alle indices gerest
-
-	//FILE* pOFile = nullptr;
-	//
-	//_wfopen_s(&pOFile, L"scene.obj", L"w+,ccs=UTF-8");
-	//
-	//if (pOFile != nullptr) 
-	//{
-	//	// it was possible to create the file for writing.
-	//	const wchar_t* text = L" # is the symbol for partial derivative.\n";
-	//	fwrite(text, wcslen(text) * sizeof(wchar_t), 1, pOFile);
-	//
-	//	fwprintf_s(pOFile, L"v %.4f %.4f %.4f\n", 0.0, 0.0, 0.0f);
-	//	fwprintf_s(pOFile, L"v %.4f %.4f %.4f\n", 0.0, 0.0, 1.0f);
-	//	fwprintf_s(pOFile, L"v %.4f %.4f %.4f\n", 0.0, 1.0, 0.0f);
-	//	fwprintf_s(pOFile, L"v %.4f %.4f %.4f\n", 0.0, 1.0, 1.0f);
-	//	fwprintf_s(pOFile, L"v %.4f %.4f %.4f\n", 1.0, 0.0, 0.0f);
-	//	fwprintf_s(pOFile, L"v %.4f %.4f %.4f\n", 1.0, 0.0, 1.0f);
-	//	fwprintf_s(pOFile, L"v %.4f %.4f %.4f\n", 1.0, 1.0, 0.0f);
-	//	fwprintf_s(pOFile, L"v %.4f %.4f %.4f\n", 1.0, 1.0, 1.0f);
-	//
-	//	fwprintf_s(pOFile, L"vn %.4f %.4f %.4f\n", 0.0f, 0.0f, 1.0f);
-	//	fwprintf_s(pOFile, L"vn %.4f %.4f %.4f\n", 0.0f, 0.0f, -1.0f);
-	//	fwprintf_s(pOFile, L"vn %.4f %.4f %.4f\n", 0.0f, 1.0f, 0.0f);
-	//	fwprintf_s(pOFile, L"vn %.4f %.4f %.4f\n", 0.0f, -1.0f, 0.0f);
-	//	fwprintf_s(pOFile, L"vn %.4f %.4f %.4f\n", 1.0f, 0.0f, 0.0f);
-	//	fwprintf_s(pOFile, L"vn %.4f %.4f %.4f\n", -1.0f, 0.0f, 0.0f);
-	//
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 1, 2, 7, 2, 5, 2);
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 1, 2, 3, 2, 7, 2);
-	//
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 1, 6, 4, 6, 3, 6);
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 1, 6, 2, 6, 4, 6);
-	//
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 3, 3, 8, 3, 7, 3);
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 3, 3, 4, 3, 8, 3);
-	//
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 5, 5, 7, 5, 8, 5);
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 5, 5, 8, 5, 6, 5);
-	//
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 1, 4, 5, 4, 6, 4);
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 1, 4, 6, 4, 2, 4);
-	//
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 2, 1, 6, 1, 8, 1);
-	//	fwprintf_s(pOFile, L"f %d//%d %d//%d %d//%d\n", 2, 1, 8, 1, 4, 1);
-	//
-	//	fclose(pOFile);
-	//	return 0;
-	//}
-	//else 
-	//{
-	//	return -1;
-	//}
-}
-
-
-void FirstPartLes1()
-{
-	// be careful not to use json in the file itself
-	const char* jsonContents = R"json( [ {"loc": [10, 15, 0] }, {"loc": [17, 21, 1] }, {"loc": [41, 33, 2] } ] )json";
-
-	rapidjson::Document jsonDoc;
-	jsonDoc.Parse(jsonContents);
-
-	using rapidjson::Value;
-
-	if (!jsonDoc.IsArray()) return;
-
-	for (Value::ConstValueIterator itr = jsonDoc.Begin(); itr != jsonDoc.End(); ++itr)
+	else
 	{
-		const Value& pos = *itr;
-		const Value& loc = pos["loc"];
-
-		if (loc.IsArray() && loc.Size() == 3)
-		{
-			const Value& xVal = loc[0];
-			const Value& yVal = loc[1];
-			const Value& zVal = loc[2];
-
-			if ((xVal.IsInt() && yVal.IsInt() && zVal.IsInt()) == false) return;
-
-			printf("\n{%d, %d, %d}\n", xVal.GetInt(), yVal.GetInt(), zVal.GetInt());
-		}
+		wprintf_s(L"Couldn't open inputfile\n");
+		Help();
+		return -1;
 	}
 
-	const wchar_t* currentWorkingDir = _wgetcwd(nullptr, 0);
-
-	if (currentWorkingDir != nullptr)
-	{
-		std::cout << "\n";
-		const std::wstring current_working_dir(currentWorkingDir);
-		std::wcout << current_working_dir << std::endl;
-		std::cout << "\n";
-	}
+	return 0;
+	
 }
 
-void MinecraftJSON()
+void WriteAwayBlock(FILE* pOutputFile, const Block& block, const int offset)
 {
+	const Vector3& position = block.position;
+
+	//write away vertices
+	fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", position.x - 0.5f, position.y - 0.5f, position.z - 0.5f);
+	fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", position.x - 0.5f, position.y - 0.5f, position.z + 0.5f);
+	fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", position.x - 0.5f, position.y + 0.5f, position.z - 0.5f);
+	fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", position.x - 0.5f, position.y + 0.5f, position.z + 0.5f);
+	fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", position.x + 0.5f, position.y - 0.5f, position.z - 0.5f);
+	fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", position.x + 0.5f, position.y - 0.5f, position.z + 0.5f);
+	fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", position.x + 0.5f, position.y + 0.5f, position.z - 0.5f);
+	fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", position.x + 0.5f, position.y + 0.5f, position.z + 0.5f);
 
 
-
+	//write away all faces
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 2, 7 + offset, 2, 5 + offset, 2);
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 2, 3 + offset, 2, 7 + offset, 2);
+	
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 6, 4 + offset, 6, 3 + offset, 6);
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 6, 2 + offset, 6, 4 + offset, 6);
+	
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + offset, 3, 8 + offset, 3, 7 + offset, 3);
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + offset, 3, 4 + offset, 3, 8 + offset, 3);
+	
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + offset, 5, 7 + offset, 5, 8 + offset, 5);
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + offset, 5, 8 + offset, 5, 6 + offset, 5);
+	
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 4, 5 + offset, 4, 6 + offset, 4);
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 4, 6 + offset, 4, 2 + offset, 4);
+	
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + offset, 1, 6 + offset, 1, 8 + offset, 1);
+	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + offset, 1, 8 + offset, 1, 4 + offset, 1);
 }
+
+void Help()
+{
+	//todo fix that you pass an extra message
+
+	std::wstringstream wss;
+
+	wss << L"\nWrong arguments\n";
+
+	wss << L"\nPossible commands:\n";
+
+	wss << L"\nMinecraftTool -i inputfile.json\n";
+
+	wss << L"MinecraftTool -i inputfile.json -o outputfile.obj\n\n";
+
+	wprintf_s(wss.str().c_str());
+}
+
