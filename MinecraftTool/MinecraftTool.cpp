451b996184be1e4b488c0cecb8ec633966e1c325
Twannes <Twannes02@gmail.com>
2023-04-22 19:35:29 +0200

    Added optimizations for no duplicate faces

--- MinecraftTool/MinecraftTool.cpp
+++ MinecraftTool/MinecraftTool.cpp
@@ -9,6 +9,8 @@
 #include <sstream>
 #include <vector>
 
+//#include "shellapi.h"
+
 //#include "vld.h"
 
 struct Vector3
@@ -30,6 +32,8 @@ std::vector<Block> g_Blocks;
 
 void Help();
 void WriteAwayBlock(FILE* pOutputFile, const Block& block, const int offset);
+void WriteNonDuplicateFaces(FILE* pOutputFile, std::vector<Block>::iterator& it, const Vector3& position, const Vector3 comparePos, const int offset, const int startFace, const int secondFace, const std::pair<int, int>& firstPair, const std::pair<int, int>& secondPair);
+
 
 int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 {
@@ -169,6 +173,10 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 									newPos.y = static_cast<float>(y.GetInt());
 									newPos.z = static_cast<float>(z.GetInt());
 
+									newBlock.position = newPos;
+
+									//wprintf_s(L"%.4f %.4f %.4f\n", newPos.x, newPos.y, newPos.z);
+
 									//when everything is correct emplace new block
 									g_Blocks.emplace_back(newBlock);
 
@@ -206,7 +214,7 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 		fwrite(text, wcslen(text) * sizeof(wchar_t), 1, pOutputFile);
 
 		//write away the material library
-		fwprintf_s(pOutputFile, L"mtllib scene.mtl\n\n");
+		fwprintf_s(pOutputFile, L"mtllib minecraftMats.mtl\n\n");
 
 		//write away all the different normals
 		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n",   0.0f, 0.0f, 1.0f);
@@ -215,6 +223,22 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n",   0.0f, -1.0f, 0.0f);
 		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n",   1.0f, 0.0f, 0.0f);
 		fwprintf_s(pOutputFile, L"vn %.4f %.4f %.4f\n\n", -1.0f, 0.0f, 0.0f);
+
+		//write away all uv coords
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",    0.25f,   0.5f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",     0.5f,   0.5f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",     0.5f,  0.75f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",    0.25f,  0.75f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",    0.75f,   0.5f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",    0.75f,  0.75f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",	    1.f,   0.5f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",	    1.f,  0.75f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",	  0.25f,    1.f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",	   0.5f,    1.f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",	    0.f,  0.75f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",      0.f,   0.5f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n",    0.25f,  0.25f);
+		fwprintf_s(pOutputFile, L"vt %.2f %.2f\n\n",    0.5f,  0.25f);
 
 		int currIdx{};
 
@@ -237,11 +261,14 @@ int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 
 		fclose(pOutputFile);
 
-		wprintf_s(L"Operation succeeded\n");
+		wprintf_s(L"\nOperation succeeded\n");
+
+		//todo make this work
+		//ShellExecute(NULL, L"open", L"explorer.exe", outputFileName.c_str(), NULL, 10);
 	}
 	else
 	{
-		wprintf_s(L"Couldn't open inputfile\n");
+		wprintf_s(L"\nCouldn't open inputfile\n");
 		Help();
 		return -1;
 	}
@@ -265,29 +292,72 @@ void WriteAwayBlock(FILE* pOutputFile, const Block& block, const int offset)
 	fwprintf_s(pOutputFile, L"v %.4f %.4f %.4f\n", position.x + 0.5f, position.y + 0.5f, position.z + 0.5f);
 
 
-	//write away all faces
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 2, 7 + offset, 2, 5 + offset, 2);
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 2, 3 + offset, 2, 7 + offset, 2);
-	
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 6, 4 + offset, 6, 3 + offset, 6);
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 6, 2 + offset, 6, 4 + offset, 6);
-	
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + offset, 3, 8 + offset, 3, 7 + offset, 3);
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + offset, 3, 4 + offset, 3, 8 + offset, 3);
-	
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + offset, 5, 7 + offset, 5, 8 + offset, 5);
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + offset, 5, 8 + offset, 5, 6 + offset, 5);
-	
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 4, 5 + offset, 4, 6 + offset, 4);
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 4, 6 + offset, 4, 2 + offset, 4);
-	
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + offset, 1, 6 + offset, 1, 8 + offset, 1);
-	fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + offset, 1, 8 + offset, 1, 4 + offset, 1);
+	if(block.isOpaque == false)
+	{
+		//write away all faces
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 2, 7 + offset, 2, 5 + offset, 2);
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 2, 3 + offset, 2, 7 + offset, 2);
+
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 6, 4 + offset, 6, 3 + offset, 6);
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 6, 2 + offset, 6, 4 + offset, 6);
+
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + offset, 3, 8 + offset, 3, 7 + offset, 3);
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 3 + offset, 3, 4 + offset, 3, 8 + offset, 3);
+
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + offset, 5, 7 + offset, 5, 8 + offset, 5);
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 5 + offset, 5, 8 + offset, 5, 6 + offset, 5);
+
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 4, 5 + offset, 4, 6 + offset, 4);
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 1 + offset, 4, 6 + offset, 4, 2 + offset, 4);
+
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + offset, 1, 6 + offset, 1, 8 + offset, 1);
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", 2 + offset, 1, 8 + offset, 1, 4 + offset, 1);
+	}
+	else
+	{
+		//make iterator
+		auto it{ std::vector<Block>::iterator{} };
+
+		//check faces behind
+		WriteNonDuplicateFaces(pOutputFile, it, position, Vector3{ 0.f,0.f,-1.f }  , offset, 1, 2, std::make_pair(7, 3), std::make_pair(5, 7));
+
+		//check faces left
+		WriteNonDuplicateFaces(pOutputFile, it, position, Vector3{ -1.f,0.f, 0.f}  , offset, 1, 6, std::make_pair(4, 2), std::make_pair(3, 4));
+
+		//check faces above
+		WriteNonDuplicateFaces(pOutputFile, it, position, Vector3{ 0.f,1.f, 0.f }  , offset, 3, 3, std::make_pair(8, 4), std::make_pair(7, 8));
+
+		//check faces right
+		WriteNonDuplicateFaces(pOutputFile, it, position, Vector3{ 1.f,0.f, 0.f }  , offset, 5, 5, std::make_pair(7, 8), std::make_pair(8, 6));
+
+		//check faces down
+		WriteNonDuplicateFaces(pOutputFile, it, position, Vector3{ 0.f,-1.f, 0.f } , offset, 1, 4, std::make_pair(5, 6), std::make_pair(6, 2));
+
+		//check faces forward
+		WriteNonDuplicateFaces(pOutputFile, it, position, Vector3{ 0.f,0.f, 1.f }  , offset, 2, 1, std::make_pair(6, 8), std::make_pair(8, 4));
+	}
+
+}
+
+void WriteNonDuplicateFaces(FILE* pOutputFile, std::vector<Block>::iterator& it, const Vector3& position, const Vector3 comparePos, const int offset, const int startFace, const int secondFace, const std::pair<int, int>& firstPair, const std::pair<int, int>& secondPair)
+{
+	it = std::find_if(begin(g_Blocks), end(g_Blocks), [&](const Block& blockLambda)
+	{
+		return blockLambda.position.x == position.x + comparePos.x && blockLambda.position.y == position.y + comparePos.y && blockLambda.position.z == position.z + comparePos.z && blockLambda.isOpaque;
+	});
+
+	if (it == g_Blocks.end())
+	{
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", startFace + offset, secondFace, firstPair.first  + offset, secondFace,  secondPair.first + offset, secondFace);
+		fwprintf_s(pOutputFile, L"f %d//%d %d//%d %d//%d\n", startFace + offset, secondFace, firstPair.second + offset, secondFace, secondPair.second + offset, secondFace);
+	}
 }
 
 void Help()
 {
 	//todo fix that you pass an extra message
+
+	//todo fix the order of writing dirt then all dirt ones
 
 	std::wstringstream wss;
 
